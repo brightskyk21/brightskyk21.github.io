ab7507169d689aa4fd4097043cc4231a103d3c9b
brightskyk21 <104341849+brightskyk21@users.noreply.github.com>
2023-11-15 13:30:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var mapOptions = {
-    center: new naver.maps.LatLng(37.628156, 127.077821),
+    center: new naver.maps.LatLng(37.629835024085, 127.07629504),
     zoom: 17
 };
 
 var map = new naver.maps.Map('map', mapOptions);
 
 var markerOptions = {
-    position: new naver.maps.LatLng(37.62818181364887, 127.07777691170485),
+    position: new naver.maps.LatLng(37.628156, 127.077821),
     map: map
 };
 
