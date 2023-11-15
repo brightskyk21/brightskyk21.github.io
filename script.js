928b445dcbc98f44fa9d1a461442a53b157409f9
brightskyk21 <104341849+brightskyk21@users.noreply.github.com>
2023-11-15 15:20:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-function openRestaurantInfo(restaurantData) {
-    var modal = document.getElementById('restaurantModal');
-    // 예시: 식당 이름과 전화번호 설정
-    document.getElementById('restaurantName').innerText = restaurantData.name;
-    document.getElementById('restaurantPhone').innerText = restaurantData.phone;
-
-    // 웹사이트가 제공되지 않은 경우 '-'로 표시
-    var website = restaurantData.website === '-' ? '#' : restaurantData.website;
-    document.getElementById('restaurantWebsite').href = website;
-
-    modal.style.display = "block";
-
-    // 모달의 닫기 버튼
-    var span = document.getElementsByClassName("close")[0];
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // 모달 외부 클릭시 닫기
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-}
-
 var mapOptions = {
     center: new naver.maps.LatLng(37.629835024085, 127.07629504),
     zoom: 17
@@ -36,10 +10,25 @@ var restaurants = [
     {
         name: "쪼매매운떡볶이",
         phone: "02-977-1120",
-        website: "-",
-        position: new naver.maps.LatLng(37.628181, 127.077827) // 위치 데이터
-    },
-    // 여기에 다른 식당 데이터 추가...
+        website: "http://jjoemae-tteokbokki.com",
+        position: new naver.maps.LatLng(37.628181, 127.077827),
+        menu: [
+            { name: "떡볶이", price: "4,000원"},
+            { name: "튀김(3개)", price: "2,000원"}
+        ]
+    }//,
+    // 새로운 식당 데이터 추가
+    //{
+        //name: "새로운식당",
+        //phone: "02-123-4567",
+        //website: "http://newrestaurant.com",
+        //position: new naver.maps.LatLng(37.630000, 127.076000),
+        //menu: [
+            //{ name: "김밥", price: "3,000원"},
+            //{ name: "라면", price: "2,500원"}
+        //]
+    //}
+    // 추가 식당 데이터...
 ];
 
 // 각 식당에 대한 마커 생성
@@ -59,3 +48,54 @@ restaurants.forEach(function(restaurant) {
         openRestaurantInfo(marker.restaurantData);
     });
 });
+
+function openRestaurantInfo(restaurantData) {
+    // 모달 요소를 찾음
+    var modal = document.getElementById('restaurantModal');
+
+    // 팝업 내용을 위한 HTML 문자열 생성
+    var modalContentHtml = `
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <div class="modal-header">
+                <h2 id="restaurantName">${restaurantData.name}</h2>
+                <p id="restaurantPhone">${restaurantData.phone}</p>
+                <a href="${restaurantData.website}" target="_blank">웹사이트</a>
+            </div>
+            <div class="modal-body">
+                <table>
+                    <tr>
+                        <th>메뉴</th>
+                        <th>가격</th>
+                    </tr>
+                    ${restaurantData.menu.map(function(item) {
+                        return `
+                            <tr>
+                                <td>${item.name}</td>
+                                <td>${item.price}</td>
+                            </tr>
+                        `;
+                    }).join('')}
+                </table>
+            </div>
+        </div>
+    `;
+
+    // 모달 내용을 업데이트
+    modal.innerHTML = modalContentHtml;
+
+    // 모달을 표시
+    modal.style.display = "block";
+
+    // 닫기 버튼 핸들러 설정
+    modal.querySelector(".close").onclick = function() {
+        modal.style.display = "none";
+    };
+
+    // 모달 외부 클릭시 닫기 핸들러 설정
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+}
