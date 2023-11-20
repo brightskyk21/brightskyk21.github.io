4dacc4fc531741b0e94130d0c573cab7a4745650
brightskyk21 <104341849+brightskyk21@users.noreply.github.com>
2023-11-20 23:45:10 +0900

    Add files via upload

---
+++ cafes.js
@@ -0,0 +1,13 @@
+var cafes = [
+    {
+        name: "메트로폴리스",
+        phone: "02-977-1120",
+        position: new naver.maps.LatLng(37.621468, 127.075125),
+        menu: [
+            { name: "소금빵", price: "4,000원"},
+            { name: "에그타르트", price: "2,000원"},
+            { name: "블랙 폴 카스테라", price: "10,000원"},
+            { name: "밀크티", price: "6,500원"}
+        ]
+    }
+]
