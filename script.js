5057b2314fc9aa621e738dcdfbcf67f869a6e802
brightskyk21 <104341849+brightskyk21@users.noreply.github.com>
2023-11-20 16:32:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,32 +5,6 @@ var mapOptions = {
 
 var map = new naver.maps.Map('map', mapOptions);
 
-// 식당 데이터 예시
-var restaurants = [
-    {
-        name: "쪼매매운떡볶이",
-        phone: "02-977-1120",
-        position: new naver.maps.LatLng(37.628181, 127.077827),
-        menu: [
-            { name: "떡볶이", price: "4,000원"},
-            { name: "튀김(3개)", price: "2,000원"},
-            { name: "순대", price: "4,000원"},
-            { name: "쪼매김밥", price: "1,500원"}
-        ]
-    }//,
-    // 새로운 식당 데이터 추가
-    //{
-        //name: "새로운식당",
-        //phone: "02-123-4567",
-        //position: new naver.maps.LatLng(37.630000, 127.076000),
-        //menu: [
-            //{ name: "김밥", price: "3,000원"},
-            //{ name: "라면", price: "2,500원"}
-        //]
-    //}
-    // 추가 식당 데이터...
-];
-
 // 각 식당에 대한 마커 생성
 restaurants.forEach(function(restaurant) {
     var markerOptions = {
