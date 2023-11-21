1489c39860eb511137111e49fb926939836970c3
brightskyk21 <104341849+brightskyk21@users.noreply.github.com>
2023-11-21 16:32:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var mapOptions = {
-    center: new naver.maps.LatLng(37.629835024085, 127.07629504),
+    center: new naver.maps.LatLng(37.627773, 127.077664),
     zoom: 17
 };
 
@@ -25,21 +25,47 @@ function createMarker(place) {
     naver.maps.Event.addListener(marker, 'click', function() {
         openInfoWindow(marker, place);
     });
+
+    currentMarkers.push(marker)
 }
 
 function openInfoWindow(marker, place) {
     var modal = document.getElementById('restaurantModal');
     var modalContent = modal.querySelector('.modal-content');
 
+    // 먼저 메뉴 헤더를 정의합니다.
+    var menuTableHtml = `
+        <table class="menu-table">
+            <tbody>
+                <tr>
+                    <th colspan="2"><i class="fas fa-clipboard-list"></i></th>
+                </tr>`;
+
+    // 그 다음에 메뉴 항목들을 매핑합니다.
+    menuTableHtml += place.menu.map(item => `
+                <tr>
+                    <td>${item.name}</td>
+                    <td class="price">${item.price}</td>
+                </tr>
+            `).join('');
+
+    // 마지막으로 테이블 닫기 태그를 추가합니다.
+    menuTableHtml += `
+            </tbody>
+        </table>`;
+
+    // modalContent의 내부 HTML을 업데이트합니다.
     modalContent.innerHTML = `
         <div class="info-window-content">
             <h3>${place.name}</h3>
-            <p>전화번호: ${place.phone}</p>
-            <ul>${place.menu.map(item => `<li>${item.name}: ${item.price}</li>`).join('')}</ul>
+            <p><i class="fas fa-phone-alt"></i> ${place.phone}</p>
+            ${menuTableHtml}
         </div>`;
 
     modal.style.display = 'block';
 }
+
+
 
 function showPlaces(places) {
     clearMarkers();
@@ -56,8 +82,10 @@ document.getElementById('showCafes').addEventListener('click', function () {
 
 window.onclick = function(event) {
     var modal = document.getElementById('restaurantModal');
-    // 모달이 열려 있고, 클릭된 요소가 모달의 내용이 아니면 모달을 닫습니다.
-    if (modal.style.display == 'block' && !modal.contains(event.target)) {
+    var modalBackground = document.querySelector('.modal-background');
+
+    // 모달 배경을 클릭했을 때만 모달을 닫습니다.
+    if (event.target === modalBackground) {
         modal.style.display = "none";
     }
-}
+};
