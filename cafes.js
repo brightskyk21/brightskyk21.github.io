2d882511881ec53aab249381919091efd19f27a1
brightskyk21 <104341849+brightskyk21@users.noreply.github.com>
2023-11-22 14:37:14 +0900

    Update cafes.js

--- cafes.js
+++ cafes.js
@@ -1,13 +1,552 @@
 var cafes = [
     {
         name: "메트로폴리스",
-        phone: "02-977-1120",
-        position: new naver.maps.LatLng(37.621468, 127.075125),
+        phone: "0507-1392-6632",
+        position: new naver.maps.LatLng(37.621480, 127.075124),
         menu: [
-            { name: "소금빵", price: "4,000원"},
-            { name: "에그타르트", price: "2,000원"},
+            { name: "소금빵", price: "변동"},
+            { name: "에그타르트", price: "변동"},
             { name: "블랙 폴 카스테라", price: "10,000원"},
             { name: "밀크티", price: "6,500원"}
         ]
-    }
+    },
+    {
+        name: "엔젤로",
+        phone: "0507-1303-9392",
+        position: new naver.maps.LatLng(37.622338, 127.076604),
+        menu: [
+            { name: "카이막+소프트바게트+꿀", price: "8,500원"},
+            { name: "실같은 페스츄리 쿠나파", price: "6,500원"},
+            { name: "발르슈트", price: "4,500원"},
+            { name: "발르슈트라뗴", price: "5,000원"}
+        ]
+    },
+    {
+        name: "시프스",
+        phone: "0507-1358-1314",
+        position: new naver.maps.LatLng(37.626265, 127.076412),
+        menu: [
+            { name: "솔티드 카라멜 슈페너", price: "5,500원"},
+            { name: "밤밤(떠먹는 밤 커피)", price: "6,000원"},
+            { name: "수수(떠먹는 옥수수 커피)", price: "6,000원"},
+            { name: "시프스 초콜릿 브라우니", price: "7,500원"}
+        ]
+    },
+    {
+        name: "에이프릴사워",
+        phone: "0507-1356-2593",
+        position: new naver.maps.LatLng(37.625329, 127.076862),
+        menu: [
+            { name: "에이프릴 선셋티", price: "5,800원"},
+            { name: "오션라떼", price: "6,300원"},
+            { name: "자두펄그린티", price: "5,800원"},
+            { name: "딸기라떼", price: "5,800원"}
+        ]
+    },
+    {
+        name: "호이폴로이 커피로스터스",
+        phone: "010-2613-5782",
+        position: new naver.maps.LatLng(37.625262, 127.076519),
+        menu: [
+            { name: "배치브루커피", price: "3,800원"},
+            { name: "카페라떼", price: "4,500원"},
+            { name: "핸드드립", price: "5,000 ~ 6,500원"},
+            { name: "소금빵", price: "3,000원"}
+        ]
+    },
+    {
+        name: "라라브레드 공릉점",
+        phone: "1800-2790",
+        position: new naver.maps.LatLng(37.624853, 127.077870),
+        menu: [
+            { name: "라라 콘파냐", price: "4,000원"},
+            { name: "딸기요거트 스무디", price: "6,800원"},
+            { name: "허니자몽티", price: "4,500원"},
+            { name: "통베이컨 토마토 파스타", price: "15,500원"}
+        ]
+    },
+    {
+        name: "트위그 페스츄리",
+        phone: "070-4012-6321",
+        position: new naver.maps.LatLng(37.623298, 127.076884),
+        menu: [
+            { name: "얼그레이", price: "3,300원"},
+            { name: "레몬마테", price: "3,300원"},
+            { name: "아메리카노", price: "2,800원"},
+            { name: "소금빵", price: "2,200원"}
+        ]
+    },
+    {
+        name: "리브인오후",
+        phone: "0507-1334-5706",
+        position: new naver.maps.LatLng(37.622219, 127.076669),
+        menu: [
+            { name: "구황작물 치즈 케이크", price: "변동"},
+            { name: "피스타치오 보틀", price: "8,800원"},
+            { name: "초코피칸 치즈 케이크", price: "8,800원"},
+            { name: "깨고구마 치즈 케이크", price: "8,600원"}
+        ]
+    },
+    {
+        name: "던모스",
+        phone: "0507-1362-5044",
+        position: new naver.maps.LatLng(37.621344, 127.078471),
+        menu: [
+            { name: "아메리카노", price: "4,000원"},
+            { name: "아인슈패너", price: "5,000원"},
+            { name: "망고 패션후르츠 에이드", price: "6,000원"},
+            { name: "치즈케이크", price: "6,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
+    {
+        name: "",
+        phone: "",
+        position: new naver.maps.LatLng(37., 127.),
+        menu: [
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,000원"},
+            { name: "", price: "5,500원"}
+        ]
+    },
 ]
