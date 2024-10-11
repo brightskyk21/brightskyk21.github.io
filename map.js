6955b1ab029577afdba94719f27b88d2d53bd93e
brightskyk21 <104341849+brightskyk21@users.noreply.github.com>
2024-10-11 23:08:02 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,7 +1,43 @@
 // 지도 옵션 설정
 var mapOptions = {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    zoom: 15 
+    center: new naver.maps.LatLng(37.50630188, 127.12139893),
+    zoom: 14
 };
 
 var map = new naver.maps.Map('map', mapOptions);
+
+var openInfoWindow = null;
+
+var jsonFilePath = 'songpa_station.json';
+
+fetch(jsonFilePath)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(station => {
+            var marker = new naver.maps.Marker({
+                position: new naver.maps.LatLng(station.위도, station.경도),
+                map: map
+            });
+
+            var infoWindow = new naver.maps.InfoWindow({
+                content: `<div style="padding:10px;">${station.대여소명}</div>`
+            });
+
+            naver.maps.Event.addListener(marker, 'click', function() {
+                if (openInfoWindow) {
+                    openInfoWindow.close();
+                }
+
+                infoWindow.open(map, marker);
+                openInfoWindow = infoWindow;
+            });
+        });
+    })
+    .catch(error => console.error('Error loading JSON data:', error));
+
+naver.maps.Event.addListener(map, 'click', function() {
+    if (openInfoWindow) {
+        openInfoWindow.close();
+        openInfoWindow = null;
+    }
+});
