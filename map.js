10cc9196e39a8e86f9583251b926a9982a68812e
brightskyk21 <104341849+brightskyk21@users.noreply.github.com>
2024-10-13 09:56:43 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,4 +1,3 @@
-// 지도 옵션 설정
 var mapOptions = {
     center: new naver.maps.LatLng(37.50630188, 127.12139893),
     zoom: 14
@@ -30,6 +29,38 @@ fetch(jsonFilePath)
 
                 infoWindow.open(map, marker);
                 openInfoWindow = infoWindow;
+            });
+        });
+
+        // 클러스터 중심점
+        var centroids = [
+            { 위도: 37.510195, 경도: 127.094944, id: 0 },
+            { 위도: 37.488963, 경도: 127.121022, id: 1 },
+            { 위도: 37.518748, 경도: 127.123260, id: 2 },
+            { 위도: 37.489792, 경도: 127.144586, id: 3 }
+        ];
+
+        centroids.forEach(centroid => {
+            var centroidMarker = new naver.maps.Marker({
+                position: new naver.maps.LatLng(centroid.위도, centroid.경도),
+                map: map,
+                icon: {
+                    content: `<div style="width:16px; height:16px; background-color:red; border-radius:50%; border: 2px solid black;"></div>`,
+                    anchor: new naver.maps.Point(8, 8)
+                }
+            });
+
+            var centroidInfoWindow = new naver.maps.InfoWindow({
+                content: `<div style="padding:10px;">Cluster Center ${centroid.id}</div>`
+            });
+
+            naver.maps.Event.addListener(centroidMarker, 'click', function() {
+                if (openInfoWindow) {
+                    openInfoWindow.close();
+                }
+
+                centroidInfoWindow.open(map, centroidMarker);
+                openInfoWindow = centroidInfoWindow;
             });
         });
     })
